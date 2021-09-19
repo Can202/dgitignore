409b62aae30d3502188ec6dd67ba494af4108d41
Can202 <mgoopazo@gmail.com>
2021-09-19 01:30:32 -0300

    detect if the input exists

--- dgitignore.py
+++ dgitignore.py
@@ -5,7 +5,8 @@ from sys import exit
 
 def main():
 	if len(sys.argv) > 1 :
-		print("creating " + sys.argv[1])
+		verifyifexist(sys.argv[1])
+		print("Creating " + sys.argv[1])
 		
 		if os.path.exists(os.path.expanduser('~') + "/.cache/dgitignore/") == False:
 			os.system("""
@@ -15,11 +16,17 @@ def main():
 			
 			""")
 		
-		os.system("echo \"$(cat ~/.cache/dgitignore/" + sys.argv[1] + ".gitignore)\" > .gitignore")
+		#os.system("echo \"$(cat ~/.cache/dgitignore/" + sys.argv[1] + ".gitignore)\" > .gitignore")
 	else:
 		sayhelp()
 def sayhelp():
 	pass
+def verifyifexist(gitignore):
+	if os.path.exists(os.path.expanduser('~') + "/.cache/dgitignore/" + gitignore + ".gitignore"):
+		print("gitignore detected")
+	else:
+		print("Error, " + gitignore + " gitignore not exists")
+		exit()
 	
 if __name__ == "__main__":
 	main()
