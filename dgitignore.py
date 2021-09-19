b81b4523d622dca3e505996a798e0bdcf80d7cb2
Can202 <mgoopazo@gmail.com>
2021-09-19 01:45:26 -0300

    list

--- dgitignore.py
+++ dgitignore.py
@@ -5,6 +5,20 @@ from sys import exit
 
 def main():
 	if len(sys.argv) > 1 :
+		
+		if sys.argv[1] == "-l" or sys.argv[1] == "--list":
+			saylist()
+			exit()
+		if sys.argv[1] == "-u" or sys.argv[1] == "--update":
+			sayupdate()
+			exit()
+		if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+			sayhelp()
+			exit()
+		if sys.argv[1] == "-v" or sys.argv[1] == "--version":
+			print("v0.1")
+			exit()
+		
 		verifyifexist(sys.argv[1])
 		print("Creating " + sys.argv[1])
 		
@@ -27,6 +41,48 @@ def verifyifexist(gitignore):
 	else:
 		print("Error, " + gitignore + " gitignore not exists")
 		exit()
+def saylist():
+	if os.path.exists(os.path.expanduser('~') + "/.cache/dgitignore/") == False:
+		os.system("""
+		
+		mkdir -p ~/.cache
+		git clone https://github.com/github/gitignore ~/.cache/dgitignore/
+		
+		""")
 	
+	files = os.listdir(os.path.expanduser('~') + "/.cache/dgitignore/")
+	
+	files = order(files)
+	
+	for i in range(len(files)):
+		if ".gitignore" in files[i]:
+			print(files[i][:-10])
+		
+	
+def sayupdate():
+	pass
+	
+def order(output):
+	lenoutput = len(output)
+	j = False
+	i = True
+
+	while i == True:
+		i = 0
+		for i in range(lenoutput):
+			if i != 0:
+				if output[i] < output[i-1]:
+					j = True
+					m = output[i-1]
+					output[i-1] = output[i]
+					output[i] = m
+		if j == True:
+			j = False
+			i = True
+		else:
+			i = False
+	
+	return output
+
 if __name__ == "__main__":
 	main()
