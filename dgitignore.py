e1493b84db0b85accae2457c100d399f90841108
Can202 <mgoopazo@gmail.com>
2021-09-19 01:23:48 -0300

    detect

--- dgitignore.py
+++ dgitignore.py
@@ -6,7 +6,16 @@ from sys import exit
 def main():
 	if len(sys.argv) > 1 :
 		print("creating " + sys.argv[1])
-		os.system("echo \"$(curl https://raw.githubusercontent.com/github/gitignore/master/" + sys.argv[1] + ".gitignore)\" > .gitignore")
+		
+		if os.path.exists("~/.cache/dgitignore/") == False:
+			os.system("""
+			
+			mkdir -p ~/.cache
+			git clone https://github.com/github/gitignore ~/.cache/dgitignore/
+			
+			""")
+		
+		os.system("echo \"$(cat ~/.cache/dgitignore/" + sys.argv[1] + ".gitignore)\" > .gitignore")
 	else:
 		sayhelp()
 def sayhelp():
